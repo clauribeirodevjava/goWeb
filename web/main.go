0a62f0bb06b4a6aa063b3c958e1234e966632d71
Claudemir Ribeiro <clauribeirodevjava43@gmail.com>
2023-10-17 23:22:57 -0300

    banco de dados

--- web/main.go
+++ web/main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type Post struct {
+	Id     int
+	Tittle string
+	Body   string
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hellow World")
+		post := Post{Id: 1, Tittle: "Unamed Post", Body: "No Content"}
+		if title := r.FormValue("title"); title != "" {
+			post.Tittle = title
+		}
+
+		t := template.Must(template.ParseFiles("templates/index.html"))
+		if err := t.ExecuteTemplate(w, "index.html", post); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
 	})
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
